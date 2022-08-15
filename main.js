65e31709854232b4dc03ae22cd62a422145dcd48
NemanjaP83 <necaintruder@gmail.com>
2022-08-15 21:00:01 +0200

    created accordion

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 import "./js/navigation"
 import "./css/main.css"
 
+
+// const answer = document.querySelectorAll(".answer")
+const questions = document.querySelectorAll(".question")
+
+questions.forEach(question => question.addEventListener("click", () => {
+    if (question.parentNode.classList.contains("active")) {
+        question.parentNode.classList.toggle("active")
+        question.nextElementSibling.classList.toggle("show")
+    } else {
+        questions.forEach(question => question.parentNode.classList.remove("active"))
+        questions.forEach(question => question.nextElementSibling.classList.remove("show"))
+        question.parentNode.classList.add("active")
+        question.nextElementSibling.classList.add("show")
+    }
+}))
